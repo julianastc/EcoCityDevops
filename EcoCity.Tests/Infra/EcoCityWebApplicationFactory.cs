ca6a6ca806186d896d8f9338e4eeeb503e411d27
Carlos Martins <carloseduardogomesmartins@outlook.com>
2026-05-10 14:41:00 -0300

    fix

--- EcoCity.Tests/Infra/EcoCityWebApplicationFactory.cs
+++ EcoCity.Tests/Infra/EcoCityWebApplicationFactory.cs
@@ -2,29 +2,16 @@ using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Mvc.Testing;
 using Microsoft.Extensions.DependencyInjection;
 using MongoDB.Driver;
-using Testcontainers.MongoDb;
 
 namespace EcoCity.Tests.Infra;
 
-public class EcoCityWebApplicationFactory : WebApplicationFactory<Program>, IAsyncLifetime
+public class EcoCityWebApplicationFactory : WebApplicationFactory<Program>
 {
-    private static readonly Lazy<MongoDbContainer> SharedContainer = new(() =>
-    {
-        var container = new MongoDbBuilder().WithImage("mongo:7.0").Build();
-        container.StartAsync().GetAwaiter().GetResult();
-        return container;
-    });
+    private static readonly string ConnectionString =
+        Environment.GetEnvironmentVariable("TEST_MONGODB_CONNECTION")
+        ?? "mongodb://localhost:27017";
 
-    public string DatabaseName { get; } = "ecocity_tests";
-    private string ConnectionString => SharedContainer.Value.GetConnectionString();
-
-    public Task InitializeAsync()
-    {
-        _ = SharedContainer.Value;
-        return Task.CompletedTask;
-    }
-
-    public new Task DisposeAsync() => Task.CompletedTask;
+    public string DatabaseName { get; } = $"ecocity_tests_{Guid.NewGuid():N}";
 
     protected override void ConfigureWebHost(IWebHostBuilder builder)
     {
